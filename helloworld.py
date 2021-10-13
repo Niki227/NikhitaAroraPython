947efed6386e95b2bc8a4a0b1dc4d41686c226ef
Niki227 <Niki227@users.noreply.github.com>
2021-10-13 13:12:58 +1100

    intial coding

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+print(" ------------------------------------------------") 
+print("|                                                |")
+print("|    O1HelloWorld                                |")
+print("|    Name : Nikhita Arora                        |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
