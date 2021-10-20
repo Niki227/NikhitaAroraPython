63e425e6e61c7c05fcb9cd2eda1878c4cbe27920
Niki227 <Niki227@users.noreply.github.com>
2021-10-21 09:20:33 +1100

    testing

--- 04GoodTeacher.py
+++ 04GoodTeacher.py
@@ -5,4 +5,10 @@ print("|    Name : Nikhita Arora                        |")
 print("|    Version : 01                                |")
 print("|                                                |")
 print(" ------------------------------------------------")
-print("Teacher's name (try Mr Horan)")
+username = input("Teacher's name (try Mr Horan) ")
+x = "Mr Horan"
+
+if username == x:
+    print("You are lucky, he is a great teacher.")
+else:
+    print(username + " is an ok teacher")
