ff6f0d337721d1bbe43ac670a2aaf449d3c6c995
Niki227 <Niki227@users.noreply.github.com>
2021-10-22 09:33:35 +1100

    testing

---
+++ 05forLoop.py
@@ -0,0 +1,9 @@
+print(" ------------------------------------------------") 
+print("|                                                |")
+print("|    O5HelloWorld                                |")
+print("|    Name : Nikhita Arora                        |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+for x in range(1, 500):
+    print(x)
