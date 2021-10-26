4e722dd980c855d92a72a2b8e500a0903e983136
Niki227 <Niki227@users.noreply.github.com>
2021-10-26 21:06:00 +1100

    done

---
+++ 06WhileLoop.py
@@ -0,0 +1,21 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    06whileLoop                                 |")
+print("|    Name : Nikhita Arora                        |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+x = input("What is the name of this subject ")
+y = "IST"
+
+while x != y:
+ print("Not Correct - try again")
+ x = input("What is the name of this subject ")
+
+else: 
+ print("")
+ print("")
+ print(" Congratulations!!")
+ print("")
+ print("")
+ print("")
